f42ee648b92e266b1cb935bb5640cdb3dbe78a64
Ildegar <illianclase12345678@gmail.com>
2025-02-14 21:09:56 +0100

    ompitimizacion

--- components/Tarjetas.js
+++ components/Tarjetas.js
@@ -1,12 +1,11 @@
 export default async function Tarjetas(filtro = '') {
-    const response = await fetch('../data/personajes.json');
+    const response = await fetch('../personajes.json');
     const personajes = await response.json();
 
     return personajes
         .filter(personaje => personaje.nombre.toLowerCase().includes(filtro.toLowerCase()))
         .map(personaje => `
-                <div class="carta" style="
-    background: url(${personaje.imagen}) center / cover;">
+                <div class="carta" style="background: url(${personaje.imagen}) center / cover;">
                     <h3>${personaje.nombre}</h3>
                     <p>${personaje.descripcion}</p>
                 </div>
